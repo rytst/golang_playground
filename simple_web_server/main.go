c5ad893e45f286a7935ed598cada06a32ddd8b12
rytst <r.saito0111@gmail.com>
2024-11-30 08:08:02 +0900

    update simple web server

--- simple_web_server/main.go
+++ simple_web_server/main.go
@@ -2,14 +2,22 @@ package main
 
 
 import (
-    "fmt"
+    "os"
+    "io"
     "net/http"
 )
 
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
+    http.HandleFunc("/", func(w http.ResponseWriter /* io.Writer */, r *http.Request) {
+        f, err := os.Open("./main.go")
+
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        defer f.Close()
+        io.Copy(w, f)
     })
     http.ListenAndServe(":8080", nil)
 }
