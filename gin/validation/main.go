0ac13bd3138f240e57f02c89f16f0cf3061dd495
rytst <r.saito0111@gmail.com>
2024-12-15 20:23:33 +0900

    go fmt

--- gin/validation/main.go
+++ gin/validation/main.go
@@ -1,34 +1,31 @@
 package main
 
-
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
-
 type User struct {
-    Id   string `json:"id" binding:"required"`
-    Name string `json:"name"`
+	Id   string `json:"id" binding:"required"`
+	Name string `json:"name"`
 }
 
-
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.POST("/users", func(c *gin.Context) {
-        var user User
-        if err := c.ShouldBindJSON(&user); err != nil {
-            c.JSON(400, gin.H{
-                "error": err.Error(),
-            })
-            return
-        }
+	router.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 
-        c.JSON(201, gin.H{
-            "message": "created successfully",
-            "data": user,
-        })
-    })
+		c.JSON(201, gin.H{
+			"message": "created successfully",
+			"data":    user,
+		})
+	})
 
-    router.Run(":8080")
+	router.Run(":8080")
 }
