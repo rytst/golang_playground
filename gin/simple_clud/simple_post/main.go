0fb77b2dabc264f170bec783513fbe8fb99a783c
rytst <r.saito0111@gmail.com>
2024-12-15 17:31:43 +0900

    add simple post method

---
+++ gin/simple_clud/simple_post/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Id   string `json:"id"`
+	Name string `json:"name`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(201, gin.H{
+			"message": "User created successfully",
+			"data":    user,
+		})
+	})
+
+	router.Run(":8080")
+}
